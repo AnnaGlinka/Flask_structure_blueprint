cb5281d25de3200a0d80966f541d6265a696b074
AnnaGlinka <aglinka8@gmail.com>
2023-07-05 21:59:58 +0200

    Add new product category

--- app/categories/routes.py
+++ app/categories/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template
+
+from flask import render_template, flash, redirect, url_for
 from app.categories import bp
 from app.extensions import db
 from app.models.category import Category
+from app.categories.forms import CategoryForm
+from flask_login import login_required
+
 
 @bp.route('/')
 def index():
@@ -9,6 +13,25 @@ def index():
     return render_template('categories/index.html', categories=categories)
 
 
-@bp.route('/categories/')
-def categories():
-    return render_template('categories/categories.html')
+@bp.route('/add', methods=['GET', 'POST'])
+@login_required
+def add_category():
+    form = CategoryForm()
+    if form.validate_on_submit():
+        category = Category.query.filter_by(name=form.name.data).first()
+        if category is None:
+            category = Category(name=form.name.data)
+            db.session.add(category)
+            db.session.commit()
+            flash("Category added successfully")
+            return redirect(url_for('categories.index'))
+        else:
+            flash("This category already exists!")
+        form.name.data = ''
+    else:
+        if form.errors:
+            flash("Validation error: " + str(form.errors))
+
+    return render_template('categories/add_category.html', form=form)
+
+
