1e21edcd5cc9581871560a7a81d402e2553ccb01
AnnaGlinka <aglinka8@gmail.com>
2023-08-06 19:24:48 +0200

    Delivery and returns page

--- app/contacts/routes.py
+++ app/contacts/routes.py
@@ -4,5 +4,9 @@ from app.contacts import bp
 
 @bp.route('/')
 def contact_us():
-
     return render_template('contact/contact_us.html')
+
+
+@bp.route('/delivery_and_return_info')
+def delivery_and_return():
+    return render_template('contact/delivery_and_return_info.html')
